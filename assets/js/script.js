7fd3edddf84c9d68507131117f2e5622bb501b12
Nirmal Subramani <kumar.n.subramani@gmail.com>
2023-06-12 08:05:11 +0000

    Modify constants and define function checkAnswer so that the user input can be compared to correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,8 @@ const animalImages = [
 const quizArea = document.getElementById('quiz-area')
 const heading = document.getElementById('heading')
 const quizImages = document.getElementById('quiz-images')
-const userGuess = document.getElementById('guess')
+const userGuess = document.getElementById("guess").value.toLowerCase();
+const correctAnswer = qImages[currentIndex].correct.toLowerCase();
 const submitButton = document.getElementById('submit')
 
 let currentIndex = 0
@@ -27,8 +28,18 @@ launchQuiz()
 function launchQuiz() {
     quizImages.src = animalImages[currentIndex].Image
     userGuess.ariaValueText= ""
-    currentIndex++
     }
+
+    function checkAnswer() {         
+        if (userGuess.value.toLowerCase(); === correctAnswer.correct.toLowerCase();) {
+          score++;
+          document.getElementById("score").textContent = `Score: ${score}/${qImages.length}`;
+          document.getElementById("answer-result").textContent = "That was correct!";
+          } else {
+          document.getElementById("answer-result").textContent = "Incorrect.";
+          }
+        }
+}
 
 
 
